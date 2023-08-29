18943473e739999d38cad9bdc9456a7cbf9a5df1
Tejas013 <tejasmadhukar6@gmail.com>
2023-08-29 15:44:53 +0530

    checkpoint

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,6 +1,10 @@
-import { Cron, Queue, StackContext } from "sst/constructs";
+import { Bucket, Cron, Job, Queue, StackContext } from "sst/constructs";
 
 export function UploadMicroserice({ stack }: StackContext) {
+
+  const video = new Bucket(stack, "video", {
+    cors: true,
+  });
 
   const poll = new Cron(stack, "Cron", {
     schedule: "rate(1 minute)",
@@ -9,8 +13,18 @@ export function UploadMicroserice({ stack }: StackContext) {
 
   const queue = new Queue(stack, "queue", {});
 
-  poll.attachPermissions(['s3']);
-  poll.bind([queue]);
+  const upload = new Job(stack, "upload", {
+    runtime: "container", // change to nodejs function only and run typescript in it 
+    architecture: "arm_64",
+    handler: "packages/UploadJob",
+    logRetention: "one_week",
+    timeout: "2 hours",
+  })
 
+  poll.attachPermissions(['s3']);
+  poll.bind([queue, upload]);
+
+  upload.attachPermissions(['s3','dynamodb']);
+  upload.bind([video])
   
 }
