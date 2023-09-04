e4fe6602ea217801debd88fa339a9277469607f6
Tejas013 <tejasmadhukar6@gmail.com>
2023-09-04 22:59:37 +0530

    updated

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -2,12 +2,12 @@ import { Cron, Queue, StackContext } from "sst/constructs";
 
 export function UploadMicroserice({ stack }: StackContext) {
 
+  const queue = new Queue(stack, "queue", {});
+
   const poll = new Cron(stack, "Cron", {
     schedule: "rate(1 minute)",
     job: "packages/functions/src/lambda.handler"
   })
-
-  const queue = new Queue(stack, "queue", {});
 
   poll.attachPermissions(['s3']);
   poll.bind([queue]);
