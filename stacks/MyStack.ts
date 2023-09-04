ce857809bf20d946c1ecd707f73fb4cc1d0950ba
Tejas013 <tejasmadhukar6@gmail.com>
2023-09-04 16:36:46 +0530

    Updated Stack

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,25 +1,35 @@
-import {Cron, Job, Queue, StackContext } from "sst/constructs";
+import { Cron, Job, Queue, StackContext } from "sst/constructs";
 
 export function UploadMicroserice({ stack }: StackContext) {
-
-  const poll = new Cron(stack, "Cron", {
-    schedule: "rate(1 minute)",
-    job: "packages/functions/src/lambda.handler"
-  })
 
   const queue = new Queue(stack, "queue", {});
 
   const upload = new Job(stack, "upload", {
-    runtime: "container", 
-    architecture: "arm_64",
+    runtime: "container",
+    architecture: "x86_64",
     handler: "packages/UploadJob",
     logRetention: "one_week",
     timeout: "2 hours",
+    container:  {
+      cmd: [ "/upload" ]
+    },
   })
+
+  const poll = new Cron(stack, "Cron", {
+    schedule: "rate(1 minute)",
+    job: {
+      function: {
+        handler: "packages/functions/src/lambda.handler",
+        environment: {
+          "UPLOAD_JOB_HANDLER_NAME": upload._jobManager.functionName
+        }
+      } 
+    } 
+  })
+
+  upload.attachPermissions(['s3','dynamodb']);
+  upload._jobManager.grantInvoke(poll.jobFunction);
 
   poll.attachPermissions(['s3']);
   poll.bind([queue, upload]);
-
-  upload.attachPermissions(['s3','dynamodb']);
-  
 }
